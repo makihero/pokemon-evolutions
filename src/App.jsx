73c5c5a659752df1bd98ff5a13335b7dd9f420d6
“Ruben <rubencallejast@gmail.com>
2023-11-26 21:23:08 -0500

    corrigiendo errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 // Components
-import React from "react";
 import Button from "./components/Button";
-import Card from "./components/card";
+import Card from "./components/Card";
 // Styles
 import "./sass/App.scss";
 // Icons
